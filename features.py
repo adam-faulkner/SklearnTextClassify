a47f9fc3c92e0ddc60c766eb4005800da7bba1b3
afaulkner <afaulkner@afaulkners-MacBook-Pro.local>
2018-01-04 16:50:02 -0500

    first commit

---
+++ features.py
@@ -0,0 +1,141 @@
+
+import numpy as np
+from collections import defaultdict
+from utils import make_tokenized_instances
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+
+class ItemSelector(BaseEstimator, TransformerMixin):
+    """
+    From http://scikit-learn.org/stable/auto_examples/hetero_feature_union.html
+
+    For data grouped by feature, select subset of data at a provided key.
+
+    The data is expected to be stored in a 2D data structure, where the first
+    index is over features and the second is over samples.  i.e.
+
+    >> len(data[key]) == n_samples
+
+    Please note that this is the opposite convention to scikit-learn feature
+    matrixes (where the first index corresponds to sample).
+
+    ItemSelector only requires that the collection implement getitem
+    (data[key]).  Examples include: a dict of lists, 2D numpy array, Pandas
+    DataFrame, numpy record array, etc.
+
+    >> data = {'a': [1, 5, 2, 5, 2, 8],
+               'b': [9, 4, 1, 4, 1, 3]}
+    >> ds = ItemSelector(key='a')
+    >> data['a'] == ds.transform(data)
+
+    ItemSelector is not designed to handle data grouped by sample.  (e.g. a
+    list of dicts).  If your data is structured this way, consider a
+    transformer along the lines of `sklearn.feature_extraction.DictVectorizer`.
+
+    Parameters
+    ----------
+    key : hashable, required
+        The key corresponding to the desired value in a mappable.
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_dict):
+        return data_dict[self.key]
+
+
+class NumExtractor(BaseEstimator, TransformerMixin):
+    """Extract features from each document for DictVectorizer"""
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, nums):
+        return [{self.key: num}
+                for num in nums]
+
+
+class FeatExtractor(BaseEstimator, TransformerMixin):
+    """Creates a list of features for a Pipeline."""
+    def __init__(self, labels):
+        self.labels = labels
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, posts):
+        dtype_ls =[(label, object) for label in self.labels]
+        features = np.recarray(shape=(len(posts),),
+                               dtype=dtype_ls)
+        for l in self.labels:
+            for i, col in posts.iterrows():
+                if l.endswith("bow"): #don't break if there's no text
+                    if not col[l]:
+                        col[l] = "x"
+                features[l][i] = col[l]
+        return features
+
+
+class MeanEmbeddingVectorizer(object):
+
+    '''From https://github.com/nadbordrozd/blog_stuff/tree/master/classification_w2v
+        Creates a feature based on the mean of the embeddings of all words
+        in a string'''
+    def __init__(self, embeddings_dic):
+        self.embeddings_dic = embeddings_dic
+        self.dim = len(embeddings_dic.itervalues().next())
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        """
+        for each embedding for each word in an instance, load into a vector (w_em_0...w_emN);
+        then, take the mean of that vector of embeddings.If w is not in the embeddings_dic,
+        then use a vector of zeros
+        """
+
+        X = make_tokenized_instances(list(X))
+        vec =  np.array([
+            np.mean([self.embeddings_dic[w] for w in words if w in self.embeddings_dic]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+        return vec
+
+class TfidfEmbeddingVectorizer(object):
+    """
+    From https://github.com/nadbordrozd/blog_stuff/tree/master/classification_w2v
+    """
+    def __init__(self, embeddings_dic):
+        self.embeddings_dic = embeddings_dic
+        self.word2weight = None
+        self.dim = len(embeddings_dic.itervalues().next())
+
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        # default idf for unknown words is the max of known idf's
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+        return self
+
+    def transform(self, X):
+        X = [x.split() for x in X]
+        return np.array([
+            np.mean([self.embeddings_dic[w] * self.word2weight[w]
+                     for w in words if w in self.embeddings_dic] or
+                    [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
---
+++ utils.py
@@ -0,0 +1,113 @@
+import re
+import numpy as np
+import spacy
+
+nlp = spacy.load('en')
+stopwords = [s.strip() for s in open("./resources/stopwords_custom.txt").readlines()]
+
+
+def clean_str(string):
+    """
+    From https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    :param string:
+    :return: string
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+def load_embedding_vectors_word2vec(vocabulary, filename, binary):
+    """
+     Fro https://github.com/nadbordrozd/blog_stuff/tree/master/classification_w2v
+     load embedding_vectors from word2vec
+    
+    :param vocabulary: 
+    :param filename: 
+    :param binary: 
+    :return: 
+    """
+    encoding = 'utf-8'
+    with open(filename, "rb") as f:
+        header = f.readline()
+        vocab_size, vector_size = map(int, header.split())
+        # initial matrix with random uniform
+        embedding_vectors = np.random.uniform(-0.25, 0.25, (len(vocabulary), vector_size))
+        if binary:
+            binary_len = np.dtype('float32').itemsize * vector_size
+            for line_no in range(vocab_size):
+                word = []
+                while True:
+                    ch = f.read(1)
+                    if ch == b' ':
+                        break
+                    if ch == b'':
+                        raise EOFError("unexpected end of input; is count incorrect or file otherwise damaged?")
+                    if ch != b'\n':
+                        word.append(ch)
+                word = str(b''.join(word), encoding=encoding, errors='strict')
+                idx = vocabulary.get(word)
+                if idx != 0:
+                    embedding_vectors[idx] = np.fromstring(f.read(binary_len), dtype='float32')
+                else:
+                    f.seek(binary_len, 1)
+        else:
+            for line_no in range(vocab_size):
+                line = f.readline()
+                if line == b'':
+                    raise EOFError("unexpected end of input; is count incorrect or file otherwise damaged?")
+                parts = str(line.rstrip(), encoding=encoding, errors='strict').split(" ")
+                if len(parts) != vector_size + 1:
+                    raise ValueError("invalid vector on line %s (is this really the text format?)" % (line_no))
+                word, vector = parts[0], list(map('float32', parts[1:]))
+                idx = vocabulary.get(word)
+                if idx != 0:
+                    embedding_vectors[idx] = vector
+        f.close()
+        return embedding_vectors
+
+def load_embedding_vectors_glove(vocabulary, filename, vector_size):
+    """
+    load embedding_vectors from GLOVE-trained embeddings
+    :param vocabulary:
+    :param filename:
+    :param vector_size:
+    :return:
+    """
+    embedding_vectors = np.random.uniform(-0.25, 0.25, (len(vocabulary), vector_size))
+    f = open(filename)
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], dtype="float32")
+        idx = vocabulary.get(word)
+        if idx != 0:
+            embedding_vectors[idx] = vector
+    f.close()
+    return embedding_vectors
+
+def make_tokenized_instances(ls, text_col=""):
+    """
+    This is mainly used to create the data used by the embeddings-dic code
+    :param ls:
+    :param text_col:
+    :return:
+    """
+    instances =[]
+    text_ls = ls
+    for text in text_ls:
+        spacy_doc = nlp(re.sub("[^a-zA-Z]+", " ", unicode(text)))
+        # don't use NLTK or Spacy's stopwords
+        tokens = [sent.string.strip().lower() for sent in spacy_doc if sent.string.strip().lower() not in stopwords]
+        instances.append(tokens)
+    return instances
